364aaf06ed70583d6c6c182eac949d0223efc582
Juuso Perälä <juuso.perala@viidakko.fi>
2019-12-12 20:04:54 +0200

    initialize javascripttimeago

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import JavascriptTimeAgo from 'javascript-time-ago';
+
+// The desired locales.
+import en from 'javascript-time-ago/locale/en';
+
+// Initialize the desired locales.
+JavascriptTimeAgo.locale(en);
 
 ReactDOM.render(<App />, document.getElementById('root'));
